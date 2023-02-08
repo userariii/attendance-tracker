c7dc7e705a2fc2bbf3d02ee14945b42dd7577664
ARINDAM BHATTACHARJEE <abhattacharjee717@gmail.com>
2023-02-08 14:55:38 +0530

    attandance tracker

---
+++ modules/footer.php
@@ -0,0 +1,10 @@
+	</div>
+		<footer>
+			<p>&copy; 2017 mAttendance. 
+				
+			</p>
+		</footer>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+</html>
---
+++ modules/studentspage.php
@@ -0,0 +1,23 @@
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 col-lg-12">
+			<h1 class="page-header">Updates from DDU</h1>  
+		</div>
+	</div>
+	
+	<div class="row">
+    <div class="col-md-12 col-lg-12">
+			<h3>Go Green Go Paperless</h3>
+			<img src="images/gogreen.jpg" class="img-responsive" alt="">
+			<p class="lead">New Initiative for hasselfree attendance taking. Attendance are made now simpler for Faculty as well as Students.</p>
+		</div>
+	</div>
+	
+	<div class="row">
+    <div class="col-md-12 col-lg-12">
+			<h3>Friends attendace are important!</h3>
+			<p>As our policy 75% attendace is compulsary. Please check your latest attendace records from "Student Section".</p>
+		</div>
+	</div>
+	
+</div>
